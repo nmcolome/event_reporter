0031280ad2de0921380aa7b5514541aa057abf01
NataliaColome <nmcolome@gmail.com>
2019-01-07 00:12:41 -0700

    Add load_content, queue_flow logic for queue and load commands

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,3 +1,45 @@
-puts "Enter a command:"
-answer = gets.chomp
-command = answer.split(" ")
+class EventManager
+  require 'pry'
+  require 'csv'
+
+  def initialize
+    @content = nil
+    @queue_count = 0
+  end
+
+  def load_content(file)
+    CSV.open "./#{file}", headers: true, header_converters: :symbol
+  end
+
+  def queue_flow(action)
+    if action == "count"
+      p @queue_count
+    elsif action == "clear"
+      @queue_count = 0
+    end
+  end
+
+  def flow_control(command)
+    if command[0].downcase == "load"
+      @content = load_content(command[1])
+    elsif command[0].downcase == "find"
+      results = @content.find_all do |row|
+        first_name = row[:first_name]
+        first_name == command[2].capitalize
+      end
+      @queue_count = results.count
+    elsif command[0].downcase == "queue"
+      queue_flow(command[1].downcase)
+    end
+  end
+end
+
+event_manager = EventManager.new
+command = ""
+while command != 'quit' do
+  puts "Enter a command:"
+  answer = gets.chomp
+  command = answer.split(" ")
+  event_manager.flow_control(command)
+end
+
